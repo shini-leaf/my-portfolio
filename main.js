59e7795f804408e37dc9ca2dd9b509ecf78de698
shini-leaf <shiniyou137@gmail.com>
2022-07-20 22:01:06 +0900

    ポートフォリオの開発を進めました

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ const leaf = document.getElementById("leaf");
 const leaves = document.getElementById("leaves");
 const clover = document.getElementById("clover");
 const flowing = document.getElementById("flowing");
+const flower = document.getElementById("flower");
+const mash = document.getElementById("mash");
+const sunFlower = document.getElementById("sunFlower");
 const leafContent = document.getElementById("leaf_content");
+const but = document.getElementById("but");
+const butContent = document.getElementById("but_content");
 
 leaf.onclick = function () {
   leafContent.textContent += "🌱";
@@ -15,6 +20,23 @@ leaves.onclick = function () {
 clover.onclick = function () {
   leafContent.textContent += "🍀";
 };
+
 flowing.onclick = function () {
   leafContent.textContent += "🍃";
+};
+
+flower.onclick = function () {
+  leafContent.textContent += "❎";
+};
+
+mash.onclick = function () {
+  leafContent.textContent += "❎";
+};
+
+sunFlower.onclick = function () {
+  leafContent.textContent += "❎";
+};
+
+but.onclick = function () {
+  butContent.textContent = "中止になるかも💦";
 };
